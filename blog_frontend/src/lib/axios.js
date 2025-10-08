39d1182f03fd1ee32bd19fb7e4ba44cab1e11c66
Beni Fissha <benifissha@gmail.com>
2025-10-08 22:53:25 +0300

    Updated base url for axios instance

--- blog_frontend/src/lib/axios.js
+++ blog_frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://blog-app-mryd.onrender.com/api',
     withCredentials: true,
     timeout: 20000,
     headers: {
